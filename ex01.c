ef5e2e320b843a89b87b9038afe51c507a31296a
leopercopo <155971351+leopercopo@users.noreply.github.com>
2024-01-08 15:14:21 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    int intero = 123456789; 
+    printf ("intero:%d\n", intero);
+
+    return 0;
+}
+
---
+++ ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float o = 20; 
+    char e = 'E'; 
+    char l = 'L'; 
+    int q = 11; 
+    printf("q = %c \n", q);
+
+    return 0;
+}
