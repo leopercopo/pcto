5078ecac19c27ab18903ece7541d52fd4fd515a8
leopercopo <155971351+leopercopo@users.noreply.github.com>
2024-01-08 16:12:36 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+    int numero1;
+    printf ("inserisci un numero:\n");
+    scanf("%d", &numero1);
+    printf("il numero inserito è : %d\n", numero1);
+    
+
+
+
+    int numero2;
+    printf ("inserisci un numero:\n");
+    scanf("%d", &numero2);
+    printf("il numero inserito è : %d\n", numero2);
+
+    int c;
+    c=numero1*numero2;
+    printf("prodotto=%d\n",c);
+    return 0;
+}
